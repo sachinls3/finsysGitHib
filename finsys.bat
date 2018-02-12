b1aef2923017bcdf72189b23136033a65455793c
sachinls3 <sachinsonawane@gmail.com>
2018-02-12 11:43:41 +0530

    Test Change 1
    Just changed the Finsys.bat from Login.feature to CreateNewCompany.feature

--- finsys.bat
+++ finsys.bat
@@ -1,3 +1,3 @@
 set projectLocation=C:\Users\Sachin\Cucumber\finsys
 cd %projectLocation%
-mvn clean test -Dcucumber.options="src/test/resources/FeatureCollections/Login.feature"
+mvn clean test -Dcucumber.options="src/test/resources/FeatureCollections/CreateNewCompany.feature"
