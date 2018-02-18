f67f8107cad64f652576cb5b921d2c8fc711c67a
sachinls3 <sachinsonawane@gmail.com>
2018-02-18 11:37:55 +0530

    test

--- src/test/java/ejagruti/finsys/pageobjects/baseclass.java
+++
@@ -1,9 +0,0 @@
-package ejagruti.finsys.pageobjects;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-public class baseclass {
-	public static WebDriver driver=null;
-	public static WebDriverWait wait=null;
-}
